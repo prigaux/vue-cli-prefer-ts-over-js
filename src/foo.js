3a5d627bd8d5b8397e5a44251fb7cb1aaa5d1b37
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2019-04-26 23:38:21 +0200

    add test case showing foo.js is prefered over foo.ts

---
+++ src/foo.js
@@ -0,0 +1,3 @@
+export function f1() {
+    return "foo.js";
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import { f1 } from './foo'
 
 new Vue({
-  render: h => h(App),
+  render: function (createElement) {
+    return createElement('span', f1())
+  },
 }).$mount('#app')
